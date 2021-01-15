bc30e2ba42bc2992f661f57f4536457d8c67fa27
jakob.bauer <jakobbauer@thesiestagroup.com>
2021-01-15 23:28:55 +0100

    #9 [feat] added step module from ngPrime  - added steps according to doc  - created first basic reactive form inputs

--- src/app/views/application/application.component.ts
+++ src/app/views/application/application.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { MessageService } from 'primeng/api';
+import { FormBuilder, FormGroup } from '@angular/forms';
 
 @Component({
   selector: 'app-application',
@@ -6,10 +8,53 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./application.component.scss']
 })
 export class ApplicationComponent implements OnInit {
+  items;
+  activeIndex = 0;
+  form1: FormGroup;
 
-  constructor() { }
+  constructor(private messageService: MessageService, private fb: FormBuilder) {
+    this.form1 = this.fb.group({
+      vorname: [],
+      nachname: [],
+      email: [],
+      telefonnummer: [],
+      adresse: []
+    });
+  }
 
   ngOnInit(): void {
+    this.items = [{
+      label: 'Kontaktdaten',
+      command: (event: any) => {
+        this.activeIndex = 0;
+      }
+    },
+      {
+        label: 'Produkte & Angebot',
+        command: (event: any) => {
+          this.activeIndex = 1;
+        }
+      },
+      {
+        label: 'Foto-Upload',
+        command: (event: any) => {
+          this.activeIndex = 2;
+        }
+      },
+      {
+        label: 'Standplatz',
+        command: (event: any) => {
+          this.activeIndex = 3;
+        }
+      },
+      {
+        label: 'Von Rechts Wegen',
+        command: (event: any) => {
+          this.activeIndex = 4;
+        }
+      }
+    ];
   }
+
 
 }
--- src/app/views/application/application.module.ts
+++ src/app/views/application/application.module.ts
@@ -3,12 +3,20 @@ import { CommonModule } from '@angular/common';
 import { ApplicationComponent } from './application.component';
 import { RouterModule } from '@angular/router';
 import { applicationRoutes } from './application.routing';
+import { StepsModule } from 'primeng/steps';
+import { ToastModule } from 'primeng/toast';
+import { MessageService } from 'primeng/api';
+import { ReactiveFormsModule } from '@angular/forms';
 
 @NgModule({
   declarations: [ApplicationComponent],
   imports: [
     CommonModule,
+    StepsModule,
+    ToastModule,
     RouterModule.forChild(applicationRoutes),
-  ]
+    ReactiveFormsModule,
+  ],
+  providers: [MessageService]
 })
 export class ApplicationModule { }
