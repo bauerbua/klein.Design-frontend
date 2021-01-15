8fb69931168c39efb5a0a4b1bd88ada6b6532e7e
jakob.bauer <jakobbauer@thesiestagroup.com>
2021-01-15 22:32:04 +0100

    #19 [feat] added mobile navbar  - transition when opening navbar

--- src/app/shared/components/navbar/navbar.component.ts
+++ src/app/shared/components/navbar/navbar.component.ts
@@ -1,26 +1,34 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnDestroy, OnInit } from '@angular/core';
 import { BrowserService } from '../../services/browser.service';
+import { Subscription } from 'rxjs';
 
 @Component({
   selector: 'app-navbar',
   templateUrl: './navbar.component.html',
   styleUrls: ['./navbar.component.scss']
 })
-export class NavbarComponent implements OnInit {
+export class NavbarComponent implements OnInit, OnDestroy {
+  subscriptions: Subscription[] = [];
   isMobile: boolean;
   isOffset: boolean;
-  active;
+  active: boolean;
 
   constructor(private browserService: BrowserService) {
-    this.browserService.isOffset.subscribe((res: boolean) => {
+    this.subscriptions.push(this.browserService.isOffset.subscribe((res: boolean) => {
       this.isOffset = res;
-    });
-    this.browserService.isMobile.subscribe((res: boolean) => {
+    }));
+    this.subscriptions.push(this.browserService.isMobile.subscribe((res: boolean) => {
       this.isMobile = res;
-    });
+    }));
   }
 
   ngOnInit(): void {
+  }
+
+  ngOnDestroy(): void {
+    this.subscriptions.forEach(sub => {
+      sub.unsubscribe();
+    });
   }
 
 }
