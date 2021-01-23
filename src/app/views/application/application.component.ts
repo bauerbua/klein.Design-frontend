e13990f78d15bafff5feb60128261bd2ec428344
jakob.bauer <jakobbauer@thesiestagroup.com>
2021-01-23 22:56:27 +0100

    #9 [feat] added two basic forms with navigation buttons  - generate forms dynamically

--- src/app/views/application/application.component.ts
+++ src/app/views/application/application.component.ts
@@ -1,6 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { MessageService } from 'primeng/api';
-import { FormBuilder, FormGroup } from '@angular/forms';
+import { FormBuilder, FormControl, FormGroup, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-application',
@@ -11,24 +11,86 @@ export class ApplicationComponent implements OnInit {
   items;
   activeIndex = 0;
   form1: FormGroup;
+  form2: FormGroup;
 
-  constructor(private messageService: MessageService, private fb: FormBuilder) {
-    this.form1 = this.fb.group({
-      vorname: [],
-      nachname: [],
-      email: [],
-      telefonnummer: [],
-      adresse: []
-    });
+  form1Config = [
+    {
+      formControlName: 'firstname',
+      type: 'text',
+      label: 'Vorname',
+      isRequired: true,
+    },
+    {
+      formControlName: 'lastname',
+      type: 'text',
+      label: 'Nachname',
+      isRequired: true,
+    },
+    {
+      formControlName: 'email',
+      type: 'email',
+      label: 'Email',
+      isRequired: true,
+    },
+    {
+      formControlName: 'phone',
+      type: 'number',
+      label: 'Telefonnummer',
+    },
+    {
+      formControlName: 'address',
+      type: 'text',
+      label: 'Adresse',
+      isRequired: true,
+    }
+  ];
+  form2Config = [
+    {
+      formControlName: 'companyName',
+      type: 'text',
+      label: 'Firmenname'
+    },
+    {
+      formControlName: 'categories',
+      type: 'text',
+      label: 'Kategorien'
+    },
+    {
+      formControlName: 'categories',
+      type: 'textarea',
+      label: 'Beschreibung'
+    },
+    {
+      formControlName: 'facebook',
+      type: 'text',
+      label: 'Facebook'
+    },
+    {
+      formControlName: 'twitter',
+      type: 'text',
+      label: 'Twitter'
+    },
+    {
+      formControlName: 'Website',
+      type: 'text',
+      label: 'Website'
+    },
+  ];
+
+  constructor(private messageService: MessageService) {
+
   }
 
   ngOnInit(): void {
-    this.items = [{
-      label: 'Kontaktdaten',
-      command: (event: any) => {
-        this.activeIndex = 0;
-      }
-    },
+    this.form1 = new FormGroup(this.generateForm(this.form1Config));
+    this.form2 = new FormGroup(this.generateForm(this.form2Config));
+    this.items = [
+      {
+        label: 'Kontaktdaten',
+        command: (event: any) => {
+          this.activeIndex = 0;
+        }
+      },
       {
         label: 'Produkte & Angebot',
         command: (event: any) => {
@@ -56,5 +118,22 @@ export class ApplicationComponent implements OnInit {
     ];
   }
 
+  generateForm(formConfig: any[]): {} {
+    const group = {};
+    formConfig.forEach(input => {
+      const required = (input.isRequired) ? [Validators.required] : null;
+      const disabled = (input.isDisabled) ? {value: '', disabled: true} : '';
+      group[input.formControlName] = new FormControl(disabled, required);
+    });
+    return group;
+  }
+
+  nextPage(): void {
+    this.activeIndex ++;
+  }
+
+  previousPage(): void {
+    this.activeIndex --;
+  }
 
 }
--- src/app/views/application/application.module.ts
+++ src/app/views/application/application.module.ts
@@ -7,6 +7,8 @@ import { StepsModule } from 'primeng/steps';
 import { ToastModule } from 'primeng/toast';
 import { MessageService } from 'primeng/api';
 import { ReactiveFormsModule } from '@angular/forms';
+import { InputTextModule } from 'primeng/inputtext';
+import { ButtonModule } from 'primeng/button';
 
 @NgModule({
   declarations: [ApplicationComponent],
@@ -16,6 +18,8 @@ import { ReactiveFormsModule } from '@angular/forms';
     ToastModule,
     RouterModule.forChild(applicationRoutes),
     ReactiveFormsModule,
+    InputTextModule,
+    ButtonModule,
   ],
   providers: [MessageService]
 })
