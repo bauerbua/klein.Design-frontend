3fa1e6f60b2c4a1e7adfd376d65c60342e927b8e
jakob.bauer <jakobbauer@thesiestagroup.com>
2021-02-23 00:47:55 +0100

    styling & positioning

--- src/app/shared/components/shared-components.module.ts
+++ src/app/shared/components/shared-components.module.ts
@@ -11,6 +11,7 @@ import { MatFormFieldModule } from '@angular/material/form-field';
 import { MatInputModule } from '@angular/material/input';
 import { MatButtonModule } from '@angular/material/button';
 import { MatIconModule } from '@angular/material/icon';
+import { FooterComponent } from './footer/footer.component';
 
 const components = [
   BaseLayoutComponent,
@@ -21,8 +22,14 @@ const components = [
 ];
 
 @NgModule({
-  declarations: components,
-  exports: components,
+  declarations: [
+    components,
+    FooterComponent
+  ],
+  exports: [
+    components,
+    FooterComponent
+  ],
     imports: [
         CommonModule,
         RouterModule,
