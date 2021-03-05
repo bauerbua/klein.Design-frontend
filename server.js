be268ee8503ee9557c0f869089302e978584af75
jakob.bauer <jakobbauer@thesiestagroup.com>
2021-03-05 23:58:45 +0100

    edited server.js to use path to dist folder

--- server.js
+++ server.js
@@ -11,10 +11,10 @@ const path = require('path');
 const app = express();
 
 app.use(requireHTTPS);
-app.use(express.static(__dirname + './dist/klein-design-frontend'));
+app.use(express.static(__dirname + '/dist/klein-design-frontend'));
 
 app.get('/*', (req, res) => {
-  res.sendFile(path.join(__dirname + './dist/klein-design-frontend/index.html'));
+  res.sendFile(path.join(__dirname + '/dist/klein-design-frontend/index.html'));
 });
 
 app.listen(process.env.PORT || 8080, function(){
