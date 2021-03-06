47f9da9cfc946b74cc3a968dcff7c0639eaa8dae
jakob.bauer <jakobbauer@thesiestagroup.com>
2021-03-06 23:17:56 +0100

    changed enpoint to use strapi in development

--- src/assets/api/api.endpoints.ts
+++ src/assets/api/api.endpoints.ts
@@ -1,5 +1,6 @@
 export const apiEndpoints = {
-  BASE_URL: 'http://localhost:1337/',
+  // BASE_URL: 'http://localhost:1337/',
+  BASE_URL: 'https://klein-design-backend.herokuapp.com/',
   EXHIBITORS: 'exhibitors',
   IMAGES: 'picture-gallery',
   NEWSLETTER: 'newsletters'
