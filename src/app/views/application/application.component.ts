91bd0a8dafbc700d36b10f7b709030659163690d
jakob.bauer <jakobbauer@thesiestagroup.com>
2021-03-15 00:26:14 +0100

    modified application form  - added sub-form-group to application config in order to send links correctly  - added items to display selected files  - completed functionality of required files in order to create an aussteller

--- src/app/views/application/application.component.ts
+++ src/app/views/application/application.component.ts
@@ -9,6 +9,7 @@ export interface FormConfig {
   type: string;
   label: string;
   isRequired?: boolean;
+  controls?: FormConfig[];
   options?: any;
   multiple?: boolean;
   pattern?: string;
@@ -44,7 +45,6 @@ export class ApplicationComponent implements OnInit {
     this.baseService.get(apiEndpoints.TAGS).subscribe(res => {
       this.availableOptions = res;
     });
-
   }
 
   getFormGroup(index: number): FormGroup {
@@ -54,17 +54,30 @@ export class ApplicationComponent implements OnInit {
   generateForm(formConfig: any[]): {} {
     const group = {};
     formConfig.forEach(input => {
-      const required = (input.isRequired) ? [Validators.required] : null;
-      const disabled = (input.isDisabled) ? {value: '', disabled: true} : '';
-      group[input.formControlName] = new FormControl(disabled, required);
+      if (input.hasOwnProperty('controls')) {
+        group[input.formControlName] = new FormGroup({});
+        input.controls.forEach( control => {
+          const required = (control.isRequired) ? [Validators.required] : null;
+          const disabled = (control.isDisabled) ? {value: '', disabled: true} : '';
+          group[input.formControlName].addControl(control.formControlName, new FormControl(disabled, required));
+        });
+      } else {
+        const required = (input.isRequired) ? [Validators.required] : null;
+        const disabled = (input.isDisabled) ? {value: '', disabled: true} : '';
+        group[input.formControlName] = new FormControl(disabled, required);
+      }
     });
     return group;
   }
 
-  onFileSelected(e): void {
-    const filesList = e.target.files;
-    for (const file of filesList) {
-      this.requestData.append('files.titelbild', file, file.name);
+  onFileSelected(e, controlName: string): void {
+    if (controlName === 'titelbild') {
+      this.requestData.append('files.titelbild', e.target.files[0], e.target.files[0].name);
+    } else if (controlName === 'fotos') {
+      const filesList = e.target.files;
+      for (const file of filesList) {
+        this.requestData.append('files.fotos', file, file.name);
+      }
     }
   }
 
@@ -98,19 +111,21 @@ export class ApplicationComponent implements OnInit {
   }
 
   sendApplication(): void {
-    const reqBody = {};
-    this.formArray.value.forEach(object => {
-      Object.assign(reqBody, object);
-    });
-    this.requestData.append('data', JSON.stringify(reqBody));
-
-    this.baseService.post(apiEndpoints.EXHIBITORS, this.requestData).subscribe(
-      res => {
-        console.log(res);
-      }, err => {
-        console.log(err);
-      }
-    );
+    if (this.formArray.valid) {
+      const reqBody = {};
+      this.formArray.value.forEach(object => {
+        Object.assign(reqBody, object);
+      });
+      this.requestData.append('data', JSON.stringify(reqBody));
+      this.baseService.post(apiEndpoints.EXHIBITORS, this.requestData).subscribe(
+        res => {
+          console.log(res);
+        }, err => {
+          console.log(err);
+        }
+      );
+    }
   }
+
 
 }
