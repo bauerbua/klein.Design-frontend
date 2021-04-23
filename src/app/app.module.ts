5c153833be3fb9781a8a5baf7b054289af833ae1
jakob.bauer <jakobbauer@thesiestagroup.com>
2021-04-24 00:59:29 +0200

    [fix] almost finishing ng universal setup

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,14 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
-import { SharedModule } from './shared/shared.module';
+import { SharedModule } from '@shared/shared.module';
 
 import { AppComponent } from './app.component';
 import { RouterModule } from '@angular/router';
 import { entryRoutes } from './app.routing';
 import { HttpClientModule } from '@angular/common/http';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
-import { ImprintComponent } from './views/imprint/imprint.component';
+import { ImprintComponent } from '@views/imprint/imprint.component';
 
 @NgModule({
     declarations: [
@@ -16,11 +16,13 @@ import { ImprintComponent } from './views/imprint/imprint.component';
         ImprintComponent,
     ],
     imports: [
-      BrowserModule,
+      BrowserModule.withServerTransition({ appId: 'serverApp' }),
       HttpClientModule,
       SharedModule,
       BrowserAnimationsModule,
-      RouterModule.forRoot(entryRoutes),
+      RouterModule.forRoot(entryRoutes, {
+    initialNavigation: 'enabled'
+}),
     ],
   providers: [],
   bootstrap: [AppComponent]
