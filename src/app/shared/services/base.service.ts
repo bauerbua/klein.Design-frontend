a3d39a563c2216c05be7848d695e529a91656401
jakob.bauer <jakobbauer@thesiestagroup.com>
2021-04-26 19:13:46 +0200

    #22 [feat] changed BASE_URL to environment variable

--- src/app/shared/services/base.service.ts
+++ src/app/shared/services/base.service.ts
@@ -1,7 +1,7 @@
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import { Observable } from 'rxjs';
-import { apiEndpoints } from '../../../assets/api/api.endpoints';
+import { environment } from '../../../environments/environment';
 
 @Injectable({
     providedIn: 'root',
@@ -11,18 +11,18 @@ export class BaseService {
     constructor(public http: HttpClient) {}
 
     get<T>(endpoint: string, options?: {}): Observable<T> {
-        return this.http.get<T>(apiEndpoints.BASE_URL + endpoint, options);
+        return this.http.get<T>(environment.BASE_URL + endpoint, options);
     }
 
     post<T>(endpoint: string, body: any, options?: {}): Observable<T> {
-        return this.http.post<T>(apiEndpoints.BASE_URL + endpoint, body, options);
+        return this.http.post<T>(environment.BASE_URL + endpoint, body, options);
     }
 
     update<T>(endpoint: string, body: any, options?: {}): Observable<T> {
-        return this.http.patch<T>(apiEndpoints.BASE_URL + endpoint, body, options);
+        return this.http.patch<T>(environment.BASE_URL + endpoint, body, options);
     }
 
     delete<T>(endpoint: string, options?: {}): Observable<T> {
-        return this.http.delete<T>(apiEndpoints.BASE_URL + endpoint, options);
+        return this.http.delete<T>(environment.BASE_URL + endpoint, options);
     }
 }
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,7 +3,9 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
-  production: false
+  production: false,
+  // BASE_URL: 'http://localhost:1337/',
+  BASE_URL: 'https://klein-design-backend.herokuapp.com/',
 };
 
 /*
